45e4e4455229b47236c3b072fb8f4a584c6b9bbc
sapho-sys <saphonkunzi@gmail.com>
2022-06-01 16:27:04 +0200

    updated!

--- books.js
+++ books.js
@@ -3,6 +3,10 @@ const bookTemplateElem = document.querySelector(".bookTemplate");
 const messageTS = document.querySelector(".messageTS");
 const messageHBS = document.querySelector(".messageHBS");
 
+const firstName="Skillo";
+const dayCount=3;
+const bookCount=2;
+
 let message = `Dear ${firstName},
 
 The ${bookCount} books you took out at the library is due in ${dayCount} days.
@@ -10,4 +14,25 @@ The ${bookCount} books you took out at the library is due in ${dayCount} days.
 Please return these books to us in the next ${dayCount} days.
 
 `
+
+messageTS.innerHTML=message;
+
+const template= Handlebars.compile(bookTemplateElem.innerHTML)
+
+messageHBS.innerHTML= template({
+	firstName : "Lindani",
+	bookCount : 3,
+	dayCount : 5,
+	books : [
+		{ 
+			title : "Lord of the Rings.", author : "J.R.R. Tolkien"
+		},
+		{ 
+			title : "Harry Potter and the Sorcerer's Stone.", author : "J.K. Rowling"
+		},
+		{ 
+			title : "Alice's Adventures in Wonderland.", author : "Lewis Carroll"
+		}
+	] 
+});
 
