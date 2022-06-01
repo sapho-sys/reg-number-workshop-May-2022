e7ccca14ee46c1a66a27568692cc89280ee680cd
avermeulen <andre.vermeulen@gmail.com>
2022-06-01 11:02:23 +0200

    reworked the instructions

--- books.js
+++ books.js
@@ -1,5 +1,13 @@
 const bookTemplateElem = document.querySelector(".bookTemplate");
 // const message = document.querySelector(".message");
-const message1 = document.querySelector(".message1");
-const message2 = document.querySelector(".message2");
+const messageTS = document.querySelector(".messageTS");
+const messageHBS = document.querySelector(".messageHBS");
+
+let message = `Dear ${firstName},
+
+The ${bookCount} books you took out at the library is due in ${dayCount} days.
+
+Please return these books to us in the next ${dayCount} days.
+
+`
 
